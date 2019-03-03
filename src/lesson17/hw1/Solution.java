c100ca10e4ccc061c69258fb6e2928bfc5a00e97
Sedzay <es281082@ukr.net>
2019-03-03 19:33:18 +0200

    fixed space

--- src/lesson17/hw1/Solution.java
+++ src/lesson17/hw1/Solution.java
@@ -2,8 +2,9 @@ package lesson17.hw1;
 
 public class Solution {
     public static void main(String[] args) {
-        String word = "It's just test 1 or 3, 5 words, when i must to get 9 words";
+        String word = "Аялот   It's    just test 1 or 3, 5 words, when i must to get 9 words лолот ";
         System.out.println(countWords(word));
+
     }
 
     public static int countWords(String input) {
@@ -13,7 +14,7 @@ public class Solution {
         String[] strings = input.split(" ");
 
         for (String string : strings) {
-            if (checkWord(string))
+            if (string != "" && checkWord(string))
                 count++;
         }
         return count;
@@ -21,11 +22,12 @@ public class Solution {
 
     private static boolean checkWord(String word) {
         char[] chars = word.toCharArray();
+
         for(char ch : chars) {
-            if(!((66 <= ch && ch <= 90) || (97 <= ch && ch <= 122))) {
-                return false;
+            if(((65 <= ch && ch <= 90) || (97 <= ch && ch <= 122) || (1040 <= ch && ch <= 1103))) {
+                return true;
             }
         }
-        return true;
+        return false;
     }
 }
