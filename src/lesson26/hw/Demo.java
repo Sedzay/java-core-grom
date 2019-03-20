3adbc390c0f0ec021beb90706c2ef874755a2f76
Sedzay <es281082@ukr.net>
2019-03-20 21:54:44 +0200

    change return value in controllers method put

---
+++ src/lesson26/hw/Demo.java
@@ -0,0 +1,8 @@
+package lesson26.hw;
+
+public class Demo {
+    public static void main(String[] args) {
+        ArrayListTest arrayListTest = new ArrayListTest();
+        arrayListTest.useList();
+    }
+}
