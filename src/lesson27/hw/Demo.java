15ecf47b8afb40ccb5e30aa24b7398ad5c489cae
Sedzay <es281082@ukr.net>
2019-03-21 21:45:24 +0200

    init commit

---
+++ src/lesson27/hw/Demo.java
@@ -0,0 +1,8 @@
+package lesson27.hw;
+
+public class Demo {
+    public static void main(String[] args) {
+        LinkedListTest linkedListTest = new LinkedListTest();
+        linkedListTest.useList();
+    }
+}
