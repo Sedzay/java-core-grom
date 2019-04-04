ef458d2a3ce9129ed6076c70768b96a9b3bb1f1f
Sedzay <es281082@ukr.net>
2019-04-04 22:34:36 +0300

    init commit

---
+++ src/lesson31/hw/Demo.java
@@ -0,0 +1,27 @@
+package lesson31.hw;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        System.out.println(solution.countSymbols("text text text"));
+        System.out.println(solution.countSymbols("124 text ext"));
+        System.out.println(solution.countSymbols("/xs*cd ivaKLC ext"));
+        System.out.println(solution.countSymbols("Иванов иван Иванович"));
+        System.out.println(solution.countSymbols("/xs*cd Петро Петрович"));
+
+        System.out.println();
+
+        System.out.println(solution.words("text"));
+        System.out.println(solution.words("texttext"));
+        System.out.println(solution.words("text text"));
+        System.out.println(solution.words("text text text"));
+        System.out.println(solution.words("text texttext"));
+        System.out.println(solution.words("text text texttext texttext"));
+        System.out.println(solution.words("text text texttext textte1xt"));
+        System.out.println(solution.words("tex/t *text te3xtt3ext -texttext"));
+
+
+    }
+}
