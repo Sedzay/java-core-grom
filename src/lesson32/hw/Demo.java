a6948b4462465e3a20211c4065fc606ce64d2917
Sedzay <es281082@ukr.net>
2019-04-07 21:20:41 +0300

    init commit

---
+++ src/lesson32/hw/Demo.java
@@ -0,0 +1,8 @@
+package lesson32.hw;
+
+public class Demo {
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        solution.readNumbers();
+    }
+}
