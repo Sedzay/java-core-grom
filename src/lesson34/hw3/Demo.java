90f55f40623c14227e7b8ea3a09c5d32094a1a76
Sedzay <es281082@ukr.net>
2019-04-16 22:54:39 +0300

    init commit

---
+++ src/lesson34/hw3/Demo.java
@@ -0,0 +1,7 @@
+package lesson34.hw3;
+
+public class Demo {
+    public static void main(String[] args) throws Exception{
+        Solution.copyFileContentApacheIO("C:\\Users\\User\\Desktop\\test1.txt", "C:\\Users\\User\\Desktop\\test3.txt");
+    }
+}
