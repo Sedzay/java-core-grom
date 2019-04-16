7078c131f94a9f833747c5327fd5a66565473e47
Sedzay <es281082@ukr.net>
2019-04-16 22:55:54 +0300

    init commit

--- src/lesson34/hw3/Demo.java
+++ src/lesson34/hw3/Demo.java
@@ -2,6 +2,7 @@ package lesson34.hw3;
 
 public class Demo {
     public static void main(String[] args) throws Exception{
-        Solution.copyFileContentApacheIO("C:\\Users\\User\\Desktop\\test1.txt", "C:\\Users\\User\\Desktop\\test3.txt");
+        Solution.copyFileContent("C:\\Users\\User\\Desktop\\test1.txt", "C:\\Users\\User\\Desktop\\test3.txt");
+        Solution.copyFileContentApacheIO("C:\\Users\\User\\Desktop\\test1.txt", "C:\\Users\\User\\Desktop\\test4.txt");
     }
 }
