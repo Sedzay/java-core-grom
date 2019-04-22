c133374bcf16a10984212debb689aeb11291211e
Sedzay <es281082@ukr.net>
2019-04-22 23:12:04 +0300

    standart method map

--- src/gromecode/lesson31/hw/Solution.java
+++ src/gromecode/lesson31/hw/Solution.java
@@ -11,15 +11,11 @@ public class Solution {
         char[] chars = text.toCharArray();
 
         for (char ch : chars) {
-            if (Character.isLetter(ch)) {
-                int count = 0;
-                for (int i = 0; i < chars.length; i++) {
-                    if (chars[i] == ch) {
-                        count++;
-                    }
-                }
-                mapCountChar.put(ch, count);
-            }
+            if (!Character.isLetter(ch))
+                continue;
+            if (mapCountChar.containsKey(ch)) {
+                mapCountChar.put(ch, mapCountChar.get(ch) + 1);
+            } else mapCountChar.put(ch, 1);
         }
         return mapCountChar;
     }
@@ -28,33 +24,28 @@ public class Solution {
     public Map<String, Integer> words(String text) {
         Map<String, Integer> mapCountWords = new HashMap<>();
 
-        String[]words = text.split(" ");
+        String[] words = text.split(" ");
 
         for (String word : words) {
-            if (isWord(word)) {
-                int count = 0;
-                for (int i = 0; i < words.length; i++) {
-                    if (words[i].equals(word)) {
-                        count++;
-                    }
-                }
-                mapCountWords.put(word, count);
-            }
+            if (!isWord(word))
+                continue;
+            if (mapCountWords.containsKey(word)) {
+                mapCountWords.put(word, mapCountWords.get(word) + 1);
+            } else mapCountWords.put(word, 1);
         }
-
         return mapCountWords;
     }
 
 
     private boolean isWord(String word) {
 
-        char[]chars = word.toCharArray();
+        char[] chars = word.toCharArray();
 
-        if(chars.length <=1 )
+        if (chars.length <= 1)
             return false;
 
         for (char ch : chars) {
-            if(!Character.isLetter(ch)) {
+            if (!Character.isLetter(ch)) {
                 return false;
             }
         }
